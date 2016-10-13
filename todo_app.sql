319eabf71d4f0da464545f48ba517aabfd923136
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-10-12 15:38:04 -1000

    selected tasks from table and updated them

--- todo_app.sql
+++ todo_app.sql
@@ -14,14 +14,38 @@ CREATE TABLE tasks (
   id serial NOT NULL PRIMARY KEY,
   title varchar(255) NOT NULL,
   description text,
-  created_at timestamp NOT NULL DEFAULT now(),
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp,
   completed boolean NOT NULL DEFAULT false
 );
 
 ALTER TABLE tasks DROP COLUMN completed;
 
-ALTER TABLE tasks ADD COLUMN completed_at timestamp DEFAULT null;
+ALTER TABLE tasks ADD COLUMN completed_at timestamp DEFAULT NULL;
 
 ALTER TABLE tasks ALTER COLUMN updated_at SET NOT NULL;
-ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT now();
+ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT CURRENT_TIMESTAMP;
+
+INSERT INTO tasks VALUES (
+  DEFAULT,
+  'Study SQL',
+  'Complete this exercise',
+  CURRENT_TIMESTAMP,
+  CURRENT_TIMESTAMP,
+  NULL
+);
+
+INSERT INTO tasks VALUES (
+  DEFAULT,
+  'Study PostgreSQL',
+  'Read all the documentation',
+  DEFAULT,
+  DEFAULT,
+  DEFAULT
+);
+
+SELECT title FROM tasks WHERE completed_at IS NULL;
+
+UPDATE tasks SET completed_at = CURRENT_TIMESTAMP WHERE title = 'Study SQL';
+
+SELECT title, description FROM tasks WHERE completed_at IS NULL;
