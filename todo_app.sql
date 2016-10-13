07a2b818ceb9fac3a6d3d8268e3287df23b5cefe
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-10-12 15:12:58 -1000

    altered a column

--- todo_app.sql
+++ todo_app.sql
@@ -14,9 +14,14 @@ CREATE TABLE tasks (
   id serial NOT NULL PRIMARY KEY,
   title varchar(255) NOT NULL,
   description text,
-  created_at timestamp NOT NULL,
+  created_at timestamp NOT NULL DEFAULT now(),
   updated_at timestamp,
-  completed boolean NOT NULL
+  completed boolean NOT NULL DEFAULT false
 );
 
 ALTER TABLE tasks DROP COLUMN completed;
+
+ALTER TABLE tasks ADD COLUMN completed_at timestamp DEFAULT null;
+
+ALTER TABLE tasks ALTER COLUMN updated_at SET NOT NULL;
+ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT now();
